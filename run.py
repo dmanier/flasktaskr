56dbaf63625b40cee4638d371793735fda0c38aa
Damien Manier <damienmanier12@gmail.com>
2015-05-10 21:59:57 -0400

    Created login page for flasktaskr.

---
+++ run.py
@@ -0,0 +1,5 @@
+__author__ = 'Damien'
+
+from views import app
+
+app.run(debug=True)
--- views.py
+++ views.py
@@ -1,11 +1,44 @@
-from flask import Flask
+import sqlite3
+from functools import wraps
+from flask import Flask, flash, redirect, render_template, \
+    request, session, url_for
 
 app = Flask(__name__)
+app.config.from_object('_config')
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args,**kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/logout/')
+def logout():
+    session.pop('logged_in', None)
+    flash('Goodbye!')
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] \
+                or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template('login.html', error=error)
+        else:
+            session['logged_in'] = True
+            flash('Welcome!')
+            return redirect(url_for('tasks'))
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
